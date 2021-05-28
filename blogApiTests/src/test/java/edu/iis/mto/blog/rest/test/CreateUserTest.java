5336276c4929f058ca095ac2f3a3fdb8db932ab9
Kupis.Piotr <222488@edu.p.lodz.pl>
2021-05-28 18:55:18 +0200

    Added tests that should return conflict code when try to create user with  email already taken

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -1,12 +1,11 @@
 package edu.iis.mto.blog.rest.test;
 
-import static io.restassured.RestAssured.given;
-
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
 
 class CreateUserTest extends FunctionalTests {
 
@@ -16,13 +15,28 @@ class CreateUserTest extends FunctionalTests {
     void createUserWithProperDataReturnsCreatedStatus() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
         given().accept(ContentType.JSON)
-               .header("Content-Type", "application/json;charset=UTF-8")
-               .body(jsonObj.toString())
-               .expect()
-               .log()
-               .all()
-               .statusCode(HttpStatus.SC_CREATED)
-               .when()
-               .post(USER_API);
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(USER_API);
+    }
+
+    @Test
+    void shouldReturnConflictStatusCodeWhenTryToCreateUserWithEmailThatAlreadyExists() {
+
+        JSONObject jsonObj = new JSONObject().put("email", "tracy1@domain.com");
+        given().accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(USER_API);
     }
 }
