65cff0f676b5ababd68329a0ef1f5894fbcabd19
Kupis.Piotr <222488@edu.p.lodz.pl>
2021-05-28 20:25:27 +0200

    Added test that should not return user when it was removed

--- blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/BlogDataFinder.java
@@ -1,14 +1,7 @@
 package edu.iis.mto.blog.domain;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import edu.iis.mto.blog.domain.model.AccountStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.User;
 import edu.iis.mto.blog.domain.repository.BlogPostRepository;
@@ -18,20 +11,26 @@ import edu.iis.mto.blog.dto.PostData;
 import edu.iis.mto.blog.dto.UserData;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.DataFinder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 @Service
 public class BlogDataFinder extends DomainService implements DataFinder {
 
     protected BlogDataFinder(UserRepository userRepository, BlogPostRepository blogPostRepository, LikePostRepository likePostRepository,
-            BlogDataMapper mapper) {
+                             BlogDataMapper mapper) {
         super(userRepository, blogPostRepository, likePostRepository, mapper);
     }
 
     @Override
     public UserData getUserData(Long userId) {
         User user = userRepository.findById(userId)
-                                  .orElseThrow(domainError(DomainError.USER_NOT_FOUND));
+                .orElseThrow(domainError(DomainError.USER_NOT_FOUND));
 
         return mapper.mapToDto(user);
     }
@@ -42,30 +41,31 @@ public class BlogDataFinder extends DomainService implements DataFinder {
                 searchString, searchString);
 
         return users.stream()
-                    .map(mapper::mapToDto)
-                    .collect(Collectors.toList());
+                .filter(user -> user.getAccountStatus() != AccountStatus.REMOVED)
+                .map(mapper::mapToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public PostData getPost(Long postId) {
         BlogPost blogPost = blogPostRepository.findById(postId)
-                                              .orElseThrow(domainError(DomainError.POST_NOT_FOUND));
+                .orElseThrow(domainError(DomainError.POST_NOT_FOUND));
         return mapper.mapToDto(blogPost);
     }
 
     @Override
     public List<PostData> getUserPosts(Long userId) {
         User user = userRepository.findById(userId)
-                                  .orElseThrow(domainError(DomainError.USER_NOT_FOUND));
+                .orElseThrow(domainError(DomainError.USER_NOT_FOUND));
 
-        if(user.getAccountStatus() != AccountStatus.CONFIRMED) {
+        if (user.getAccountStatus() != AccountStatus.CONFIRMED) {
             throw new DomainError("USER_NOT_CONFIRMED");
         }
 
         List<BlogPost> posts = blogPostRepository.findByUser(user);
         return posts.stream()
-                    .map(mapper::mapToDto)
-                    .collect(Collectors.toList());
+                .map(mapper::mapToDto)
+                .collect(Collectors.toList());
     }
 
 }
